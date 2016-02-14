595b0749656b9c081fc5491c4aae36bd5fc988e9
jleewebdev <lee45276@gmail.com>
2016-02-14 13:16:28 +0800

    Add Events.add function

--- public/javascripts/events.js
+++ public/javascripts/events.js
@@ -5,16 +5,46 @@ $(function() {
   Handlebars.registerPartial("event", $("#event").html());
 
   var Events = {
-    render: function(events) {
-      $("#events_list").html(events_template({events: events}));
+    collection: [],
+    $events_list: $("#events_list"),
+
+    add: function (events) {
+      var self = this;
+      events = _.isArray(events) ? events : [events]
+
+      events.forEach(function(event) {
+        self.collection.push(event);
+      });
+
+      self.render();
+    },
+
+    render: function() {
+      this.$events_list.html(events_template({events: this.collection}));
     }
-  }
+  } // Events
+
+  $("form").on("submit", function(e) {
+    e.preventDefault();
+    var $f = $(this);
 
     $.ajax({
-      url: "/events",
+      url: $f.attr("action"),
+      type: $f.attr("method"),
+      data: $f.serialize(),
 
-      success: function(data) {
-        Events.render(data);
+      success: function(event) {
+        Events.add(event);
       }
+
     })
+  });
+
+  $.ajax({
+    url: "/events",
+
+    success: function(events) {
+      Events.add(events);
+    }
+  });
 });
